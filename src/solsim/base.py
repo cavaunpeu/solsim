d323963c7372d36b7a28e302bf54209a9cf772c0
Will Wolf <williamabrwolf@gmail.com>
2022-01-23 20:10:35 -0500

    add initialStep method to base simulation

--- src/solsim/base.py
+++ src/solsim/base.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod, abstractproperty
-from typing import Dict, Set
+from typing import Dict, Set, List, Any
 
 class BaseSystem(ABC):
 
   @abstractmethod
-  def step(self) -> Dict:
+  def initialStep(self) -> Dict:
+    raise NotImplementedError
+
+  @abstractmethod
+  def step(self, state: Dict[str, Any], history: List[Dict[str, Any]]) -> Dict:
     raise NotImplementedError
