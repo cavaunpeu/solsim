55d2c5d4feefddb1ea2e18177479f5ae58c35566
Will Wolf <williamabrwolf@gmail.com>
2022-01-23 20:10:49 -0500

    update lotka-volterra simulation

--- examples/lotka_volterra/run.py
+++ examples/lotka_volterra/run.py
@@ -5,13 +5,15 @@ from solsim.simulation import Simulation
 if __name__ == '__main__':
   simulation = Simulation(
     system=LotkaVolterraSystem(
-      population_size=1,
-      food_supply=2
+      population_size=50,
+      food_supply=1000,
+      reproduction_rate=.01,
+      consumption_rate=.1,
     ),
     watchlist=(
       'population_size',
       'food_supply'
     ),
-    n_steps=1
+    n_steps=5
   )
   results = simulation.run()
--- examples/lotka_volterra/system.py
+++ examples/lotka_volterra/system.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from solsim.base import BaseSystem
 
 
@@ -6,13 +7,25 @@ class LotkaVolterraSystem(BaseSystem):
   def __init__(
     self,
     population_size,
-    food_supply
+    food_supply,
+    reproduction_rate,
+    consumption_rate,
   ):
     self.population_size = population_size
     self.food_supply = food_supply
+    self.reproduction_rate = reproduction_rate
+    self.consumption_rate = consumption_rate
 
-  def step(self):
+  def initialStep(self) -> Dict:
     return {
       'population_size': self.population_size,
-      'food_supply': self.food_supply
+      'food_supply': self.food_supply,
+    }
+
+  def step(self, state, history) -> Dict:
+    population_size = max(state['population_size'] + self.reproduction_rate * state['food_supply'], 0)
+    food_supply = max(state['food_supply'] - self.consumption_rate * state['population_size'], 0)
+    return {
+      'population_size': population_size,
+      'food_supply': food_supply
     }
