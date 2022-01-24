bffd1e49b2a8caac98fc125480344670e844c85c
Will Wolf <williamabrwolf@gmail.com>
2022-01-24 14:45:00 -0500

    add instruction to initialize maker, taker associated token accounts

--- examples/simple_escrow/system.py
+++ examples/simple_escrow/system.py
@@ -2,13 +2,16 @@ import os
 from typing import Dict
 
 from anchorpy import create_workspace, close_workspace, Context
+from solana.keypair import Keypair
 from solana.publickey import PublicKey
+from spl.token.instructions import get_associated_token_address
 
 from solsim.base import BaseSolanaSystem
 from solsim.constants import (
   SYSVAR_RENT_PUBKEY,
   TOKEN_PROGRAM_ID,
-  SYS_PROGRAM_ID
+  SYS_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID
 )
 
 
@@ -26,9 +29,13 @@ class SimpleEscrowSystem(BaseSolanaSystem):
   def _init_addresses(self):
     self.foo_coin_mint, self.foo_coin_mint_bump = PublicKey.find_program_address([bytes("foo", encoding='utf8')], self._escrow_program.program_id)
     self.bar_coin_mint, self.bar_coin_mint_bump = PublicKey.find_program_address([bytes("bar", encoding='utf8')], self._escrow_program.program_id)
+    self.maker, self.taker = Keypair(), Keypair()
 
   async def _initialize_escrow(self):
     await self._init_mints()
+    await self._init_associated_token_accounts()
+    await self._init_maker_assoc_token_accounts()
+    await self._init_taker_assoc_token_accounts()
 
   async def _init_mints(self):
     await self._escrow_program.rpc['init_mints'](
@@ -43,6 +50,50 @@ class SimpleEscrowSystem(BaseSolanaSystem):
           'rent': SYSVAR_RENT_PUBKEY,
           'system_program': SYS_PROGRAM_ID
         }
+      )
+    )
+
+  async def _init_associated_token_accounts(self):
+    self.maker_foo_coin_assoc_token_acct = get_associated_token_address(self.maker.public_key, self.foo_coin_mint)
+    self.maker_bar_coin_assoc_token_acct = get_associated_token_address(self.maker.public_key, self.bar_coin_mint)
+    self.taker_foo_coin_assoc_token_acct = get_associated_token_address(self.taker.public_key, self.foo_coin_mint)
+    self.taker_bar_coin_assoc_token_acct = get_associated_token_address(self.taker.public_key, self.bar_coin_mint)
+
+  async def _init_maker_assoc_token_accounts(self):
+    await self._escrow_program.rpc['init_maker_assoc_token_accts'](
+      ctx=Context(
+        accounts={
+          'foo_coin_mint': self.foo_coin_mint,
+          'bar_coin_mint': self.bar_coin_mint,
+          'maker_foo_coin_assoc_token_acct': self.maker_foo_coin_assoc_token_acct,
+          'maker_bar_coin_assoc_token_acct': self.maker_bar_coin_assoc_token_acct,
+          'payer': self.payer,
+          'maker': self.maker.public_key,
+          'token_program': TOKEN_PROGRAM_ID,
+          'associated_token_program': ASSOCIATED_TOKEN_PROGRAM_ID,
+          'rent': SYSVAR_RENT_PUBKEY,
+          'system_program': SYS_PROGRAM_ID
+        },
+        signers=[self.maker]
+      )
+    )
+
+  async def _init_taker_assoc_token_accounts(self):
+    await self._escrow_program.rpc['init_taker_assoc_token_accts'](
+      ctx=Context(
+        accounts={
+          'foo_coin_mint': self.foo_coin_mint,
+          'bar_coin_mint': self.bar_coin_mint,
+          'taker_foo_coin_assoc_token_acct': self.taker_foo_coin_assoc_token_acct,
+          'taker_bar_coin_assoc_token_acct': self.taker_bar_coin_assoc_token_acct,
+          'payer': self.payer,
+          'taker': self.taker.public_key,
+          'token_program': TOKEN_PROGRAM_ID,
+          'associated_token_program': ASSOCIATED_TOKEN_PROGRAM_ID,
+          'rent': SYSVAR_RENT_PUBKEY,
+          'system_program': SYS_PROGRAM_ID
+        },
+        signers=[self.taker]
       )
     )
 
