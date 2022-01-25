15b766911836b0707be51d2d314952928ed567f5
Will Wolf <williamabrwolf@gmail.com>
2022-01-25 17:03:27 -0500

    make system.py pass mypy --strict check

--- src/solsim/system.py
+++ src/solsim/system.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Awaitable, Dict, Set, List, Any, Union
+from typing import Awaitable, Dict, Optional, Set, List, Any, Union
 
 from anchorpy import create_workspace, close_workspace
 from solana.publickey import PublicKey
 from solana.rpc.api import Client
+from solana.rpc import commitment
 
 from solsim.type import StateType
 
@@ -23,16 +24,12 @@ class BaseSolanaSystem(BaseSystem):
 
     SOLANA_CLUSTER_URI = "http://127.0.0.1:8899"
 
-    def __init__(self, workspace_dir, client=None):
+    def __init__(self, workspace_dir: str, client: Optional[Client] = None) -> None:
         self.workspace = create_workspace(workspace_dir)
         self.client = client or Client(self.SOLANA_CLUSTER_URI)
 
-    async def tearDown(self) -> Awaitable[Any]:
+    async def tearDown(self) -> None:
         await close_workspace(self.workspace)
 
-    def get_token_account_balance(
-        self, pubkey: PublicKey, commitment="confirmed"
-    ) -> float:
-        return self.client.get_token_account_balance(pubkey, commitment)["result"][
-            "value"
-        ]["uiAmount"]
+    def get_token_account_balance(self, pubkey: PublicKey, commitment: Optional[commitment.Commitment] = commitment.Confirmed) -> float:
+        return float(self.client.get_token_account_balance(pubkey, commitment)["result"]["value"]["uiAmount"])
