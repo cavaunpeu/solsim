8a2b84a70e63f749c1e903c1fdc0267d27de0fe4
Will Wolf <williamabrwolf@gmail.com>
2022-01-25 08:24:49 -0500

    lower number of escrows and steps

--- examples/simple_escrow/run.py
+++ examples/simple_escrow/run.py
@@ -9,14 +9,14 @@ def main():
     system=SimpleEscrowSystem(
       workspace_dir=os.path.join(os.path.dirname(__file__), 'workspace'),
       init_assoc_token_acct_balance=100,
-      num_escrows=5
+      num_escrows=3
     ),
     watchlist=(
       'num_swaps',
       'mean_swap_amount',
       'mean_balance_spread'
     ),
-    n_steps=5
+    n_steps=3
   )
   results = simulation.run()
 
