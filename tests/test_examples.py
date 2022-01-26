80220bea5a6f3875e110784e18f7fa8dd9aea87e
Will Wolf <williamabrwolf@gmail.com>
2022-01-26 14:57:04 -0500

    mark test_drunken_escrow as uses_solana_localnet and omit from github workflow

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,9 +1,13 @@
+import pytest
+
+
 def test_lotka_volterra():
     from examples.lotka_volterra.run import main
 
     main()
 
 
+@pytest.mark.uses_solana_localnet
 def test_drunken_escrow():
     from examples.drunken_escrow.run import main
 
