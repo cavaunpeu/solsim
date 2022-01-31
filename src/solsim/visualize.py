d32bd1c881015f6250647eeef86214b3c3c41684
Will Wolf <williamabrwolf@gmail.com>
2022-01-31 11:25:48 -0500

    add logo and metadata

--- src/solsim/visualize.py
+++ src/solsim/visualize.py
@@ -8,13 +8,26 @@ from solsim.simulation import Simulation
 
 results = feather.read_dataframe(os.environ['SOLSIM_RESULTS_PATH'])
 idx_cols = Simulation.INDEX_COLS
+watched_vars = [col for col in results.columns if col not in idx_cols]
+
+# Sidebar
+
+st.sidebar.image(os.path.join(os.path.dirname(__file__), '../../logo.png'))
+
+st.sidebar.markdown("# 👉 Select quantities")
+quantities = st.sidebar.multiselect(
+    "State variables or KPIs to explore",
+    watched_vars
+)
+st.sidebar.markdown(f"""
+    # 🧮 Metadata
+
+    - **Runs: {results["run"].max() + 1}**
+    - **Steps per run: {results["step"].max() + 1}**
+    - **Watched quantities: {len(watched_vars)}**
+""")
 
 st.markdown("# 🔬 Results Explorer")
-
-quantities = st.sidebar.multiselect(
-    "Quantities to plot",
-    [col for col in results.columns if col not in idx_cols]
-)
 
 if quantities:
 
