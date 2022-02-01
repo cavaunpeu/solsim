6e109cb67953983f6cc90045669cdcd4137048af
Will Wolf <williamabrwolf@gmail.com>
2022-02-01 11:36:00 -0500

    appease black

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,24 +1,22 @@
 from solsim.simulation import Simulation
 
 
-
-
 def mock_run_method(num_runs, visualize_results):
     return num_runs, visualize_results
 
 
 def test_cli_command_list():
     simulation = Simulation(system=None, watchlist=(), n_steps=1)
-    expected_cli_commands = ['run']
+    expected_cli_commands = ["run"]
     actual_cli_commands = [cmd.callback.__name__ for cmd in simulation.cli.registered_commands]
     assert actual_cli_commands == expected_cli_commands
 
 
 def test_cli_run_method(mocker):
-    mocker.patch.object(Simulation, 'run', side_effect=mock_run_method)
+    mocker.patch.object(Simulation, "run", side_effect=mock_run_method)
     simulation = Simulation(system=None, watchlist=(), n_steps=1)
     cli_commands = simulation.cli.registered_commands
-    cli_run_cmd, = [cmd.callback for cmd in cli_commands if cmd.callback.__name__ == 'run']
+    (cli_run_cmd,) = [cmd.callback for cmd in cli_commands if cmd.callback.__name__ == "run"]
     args = 123, True
     assert simulation.run(*args) == cli_run_cmd(*args)
     assert Simulation.run.mock_calls == [mocker.call(*args)] * 2
